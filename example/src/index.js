e3f5ec26dcf724b85cd6b50cda7b3bf18c277da7
Albert Calasanz <acalasanzs@gmail.com>
2022-10-19 12:39:58 +0200

    example

--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,20 @@
 import "./style.css";
 import AlgoFrame from "algoframe";
-const container = document.querySelector(".container");
+import * as BezierEasing from "bezier-easing";
+const delay = 500;
+
+const box = document.querySelector(".container .box");
+const bezierValues = [0.91, 0.52, 0, 1.02];
+let animation = new AlgoFrame(
+  1500,
+  delay,
+  BezierEasing(...bezierValues),
+  0,
+  100
+);
+document.querySelector("h2").textContent =
+  "(" + bezierValues.join(", ") + ")" + " + " + delay + "ms";
+
+animation.run((value) => {
+  box.style.left = `calc(${value}% - ${box.offsetWidth * (value / 100)}px)`;
+});
