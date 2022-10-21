4799b9d7762d8fa85b8de623af5a3f00af404ba6
Albert Calasanz <acalasanzs@gmail.com>
2022-10-21 14:25:14 +0200

    preRelease

--- example/src/index.js
+++ example/src/index.js
@@ -14,7 +14,6 @@ let animation = new AlgoFrame(
 );
 document.querySelector("h2").textContent =
   "(" + bezierValues.join(", ") + ")" + " + " + delay + "ms";
-
 animation
   .run((value) => {
     box.classList.add("run");
