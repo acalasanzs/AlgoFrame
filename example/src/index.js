c02bf6c1e9c3ab536c54f616d7c464655919b0d4
Albert Calasanz <acalasanzs@gmail.com>
2022-10-21 14:39:47 +0200

    2.0.1

--- example/src/index.js
+++ example/src/index.js
@@ -14,8 +14,11 @@ let animation = new AlgoFrame(
 );
 document.querySelector("h2").textContent =
   "(" + bezierValues.join(", ") + ")" + " + " + delay + "ms";
+
+animation.FPS = 75;
 animation
-  .run((value) => {
+  .run((value, easedProgress, stats) => {
+    console.log(stats.frame);
     box.classList.add("run");
     box.style.left = `calc(${value}% - ${box.offsetWidth * (value / 100)}px)`;
     box.textContent = value.toFixed(1) + "%";
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import { EasingFunctions } from "./utils";
 class AlgoFrame {
-  constructor(duration, starttime, easing, startX, endX) {
+  constructor(
+    duration,
+    starttime,
+    easing,
+    startX,
+    endX,
+    FPS = null,
+    loop = false
+  ) {
     if (typeof easing !== "function") {
       this.easing = EasingFunctions[easing];
     } else {
@@ -14,36 +22,85 @@ class AlgoFrame {
     this.startX = startX;
     this.endX = endX;
     this.next = undefined;
+
+    this._FPS = FPS;
+    this.frameDelay = 1000 / this._FPS;
+    this.frameRate = 0;
+    this.frame = -1;
+
+    this.loop = loop;
+  }
+  get FPS() {
+    return this._FPS ? 1000 / this._FPS : null;
+  }
+  set FPS(value) {
+    const FPS = parseFloat(value);
+    if (FPS) {
+      this._FPS = FPS;
+      this.frameDelay = 1000 / FPS;
+      // this.frame = -1;
+      // this.starttime = null;
+    } else throw new Error("Not a valid Number");
+  }
+  restart(callback) {
+    this.frame = -1;
+    this.starttime = null;
+    return this.run(callback);
   }
   run(callback) {
     let left;
     let currenttime = 0;
     let last = 0;
+
+    if (this.loop) {
+      this.next = this.restart.bind(this, callback);
+    }
     function animate(timestamp) {
       last = last + (timestamp - last) - currenttime;
       currenttime = timestamp;
+      let condition, seg;
+      if (this._FPS) {
+        seg = Math.floor((timestamp - this.starttime) / this.frameDelay); // calc frame no.
+        condition = Boolean(seg > this.frame);
+      } else {
+        condition = true;
+        this.frame++;
+        seg = this.frame;
+      }
 
+      const runtime = timestamp - this.startanimationtime;
+      const relativeProgress = runtime / this.duration;
+
+      const easedProgress = this.easing(relativeProgress);
       if (!this.startanimationtime && this.starttime === 0) {
         this.startanimationtime = timestamp;
       } else if (this.starttime > 0) {
+        this.startanimationtime = timestamp;
         this.starttime =
           this.starttime - last < last * 0.7 ? 0 : this.starttime - last;
         requestAnimationFrame(animate.bind(this));
         return;
       }
-      const runtime = timestamp - this.startanimationtime;
-      const relativeProgress = runtime / this.duration;
 
-      const easedProgress = this.easing(relativeProgress);
+      if (condition) {
+        this.frame = seg;
 
-      left = (this.endX - this.startX) * Math.min(easedProgress, 1);
-      callback(left + this.startX, easedProgress, last, currenttime);
-
+        left = (this.endX - this.startX) * Math.min(easedProgress, 1);
+        callback(left + this.startX, easedProgress, {
+          lastFrame: last,
+          currentTime: currenttime,
+          frame: this.frame,
+        });
+      }
       if (!this.stop) {
         if (runtime < this.duration) {
           requestAnimationFrame(animate.bind(this));
         } else if (runtime - last * 0.7 < this.duration) {
-          callback(this.endX, 1, last, currenttime);
+          callback(this.endX, 1, {
+            lastFrame: last,
+            currentTime: currenttime,
+            frame: this.frame,
+          });
           this.next?.();
         } else {
           this.next?.();
