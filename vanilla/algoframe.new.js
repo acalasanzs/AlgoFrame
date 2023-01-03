13e0ccb5720e42826a167bc40cfe2ba5f0fe5a87
Albert Calasanz <acalasanzs@gmail.com>
2023-01-03 14:50:25 +0100

    3.4.1

--- vanilla/algoframe.new.js
+++ vanilla/algoframe.new.js
@@ -1,4 +1,34 @@
-import { EasingFunctions } from './utils';
+const EasingFunctions = {
+  // no easing, no acceleration
+  linear: t => t,
+  // accelerating from zero velocity
+  easeInQuad: t => t * t,
+  // decelerating to zero velocity
+  easeOutQuad: t => t * (2 - t),
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: t => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  // accelerating from zero velocity
+  easeInCubic: t => t * t * t,
+  // decelerating to zero velocity
+  easeOutCubic: t => --t * t * t + 1,
+  // acceleration until halfway, then deceleration
+  easeInOutCubic: t =>
+    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+  // accelerating from zero velocity
+  easeInQuart: t => t * t * t * t,
+  // decelerating to zero velocity
+  easeOutQuart: t => 1 - --t * t * t * t,
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: t => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t),
+  // accelerating from zero velocity
+  easeInQuint: t => t * t * t * t * t,
+  // decelerating to zero velocity
+  easeOutQuint: t => 1 + --t * t * t * t * t,
+  // acceleration until halfway, then deceleration
+  easeInOutQuint: t =>
+    t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t,
+}; // CONCATENATED MODULE: ./index.js
+
 class AlgoFrame {
   constructor(
     duration,
@@ -90,6 +120,7 @@ class AlgoFrame {
         if (easedProgress >= this._next.time) {
           this._next._.startanimationtime =
             params.timestamp + this._next._._starttime;
+          console.log(this._next._.starttime, params.timestamp);
           this._next._.run(this._next.callback);
           this._running.shift();
           this.nextTime();
@@ -221,7 +252,3 @@ class AlgoFrame {
     }
   }
 }
-// const anim = new AlgoFrame(2500, 2000, "easeInQuad", 50, 150);
-// anim.run((x) => console.log(x));
-
-export default AlgoFrame;
---
+++ vanilla/main.js
@@ -0,0 +1,45 @@
+const delay = 500;
+
+const box = document.querySelector('.container .box');
+const stats = document.querySelector('p');
+
+let animation = new AlgoFrame(5000, delay, 'easeInQuad', 0, 100);
+document.querySelector('h2').textContent = 'easeInQuad';
+animation.FPS = 60;
+let boxes = 0;
+[...box.parentNode.childNodes].forEach(c =>
+  c.className === 'box' ? (boxes += 1) : (boxes += 0)
+);
+const timeline = [];
+box.parentNode.childNodes.forEach((node, i) => {
+  if (node.className !== 'box') return;
+  node.style.height = (100 / boxes) * 0.75 + '%';
+  document.documentElement.style.setProperty(
+    '--size',
+    node.offsetHeight + 'px'
+  );
+  const duration = 500;
+
+  timeline.push({
+    time: (i + 1) / (box.parentNode.childNodes.length + 1),
+    duration,
+    easing: null,
+    startX: null,
+    endX: null,
+    run: function (value, eased) {
+      console.log(eased);
+      this.classList.add('run');
+      this.style.left = `calc(${value}% - ${
+        this.offsetWidth * (value / 100)
+      }px)`;
+      this.textContent = value.toFixed(1) + '%';
+    }.bind(node),
+    finally: _ => node.classList.remove('run'),
+  });
+});
+// console.log(timeline);
+function theRealCallback(value, easedProgress) {
+  document.querySelector('h2').textContent =
+    Math.round(easedProgress * 100) + '%';
+}
+animation.timeline(timeline, theRealCallback).run();
