91c7900130c1ae5836574024ee8c8d60016fd664
Albert Calasanz <acalasanzs@gmail.com>
2023-01-03 13:28:52 +0100

    3.4.0!

--- example/src/index.js
+++ example/src/index.js
@@ -7,13 +7,21 @@ const delay = 500;
 const box = document.querySelector('.container .box');
 const stats = document.querySelector('p');
 
-let animation = new AlgoFrame(1500, delay, 'linear', 0, 100);
+let animation = new AlgoFrame(500, delay, 'easeInQuad', 0, 100);
 document.querySelector('h2').textContent = 'easeInQuad';
 animation.FPS = 60;
-
+let boxes = 0;
+[...box.parentNode.childNodes].forEach(c =>
+  c.className === 'box' ? (boxes += 1) : (boxes += 0)
+);
 const timeline = [];
 box.parentNode.childNodes.forEach((node, i) => {
   if (node.className !== 'box') return;
+  node.style.height = (100 / boxes) * 0.75 + '%';
+  document.documentElement.style.setProperty(
+    '--size',
+    node.offsetHeight + 'px'
+  );
   const duration = 500;
 
   timeline.push({
@@ -22,8 +30,8 @@ box.parentNode.childNodes.forEach((node, i) => {
     easing: null,
     startX: null,
     endX: null,
-    run: function (value, ease) {
-      console.log(ease);
+    run: function (value, eased) {
+      console.log(eased);
       this.classList.add('run');
       this.style.left = `calc(${value}% - ${
         this.offsetWidth * (value / 100)
@@ -34,7 +42,8 @@ box.parentNode.childNodes.forEach((node, i) => {
   });
 });
 // console.log(timeline);
-function theRealCallback(value) {
-  //console.log(value);
+function theRealCallback(value, easedProgress) {
+  document.querySelector('h2').textContent =
+    Math.round(easedProgress * 100) + '%';
 }
-animation.timeline(timeline, theRealCallback).run();
+animation.timeline(timeline, theRealCallback);
